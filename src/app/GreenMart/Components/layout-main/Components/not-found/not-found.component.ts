0c51fe985581de11d0415e283fa187af00a8e989
YOmnAA98 <yomna.raafat98@gmail.com>
2024-10-07 13:34:42 +0300

    code contact-us component

--- src/app/GreenMart/Components/layout-main/Components/not-found/not-found.component.ts
+++ src/app/GreenMart/Components/layout-main/Components/not-found/not-found.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MainNavComponent } from "../main-nav/main-nav.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [MainNavComponent, FooterComponent],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css'
 })
