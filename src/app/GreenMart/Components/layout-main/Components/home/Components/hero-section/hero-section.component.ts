b9d489882d4f695bdacb7365b690546ca2aa6cc0
rahmawahab <rahmosh170@gmail.com>
2024-10-12 17:10:57 +0300

    add routerLinks to footer,hero and about

--- src/app/GreenMart/Components/layout-main/Components/home/Components/hero-section/hero-section.component.ts
+++ src/app/GreenMart/Components/layout-main/Components/home/Components/hero-section/hero-section.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-hero-section',
   standalone: true,
-  imports: [],
+  imports: [RouterModule]  ,
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.css'
 })
