b0367f5dd2d696e8b5483916a28ba94ba209fd86
Abulftooh <mahmoudaboalftooh@gmail.com>
2024-10-15 18:19:12 +0200

    update

--- src/app/GreenMart/Shared/Interfaces/cart.ts
+++ src/app/GreenMart/Shared/Interfaces/cart.ts
@@ -1,9 +1,9 @@
 import { Products } from "./products";
 
 export interface Cart {
-  price: any;
-  qty: any;
-  productId: number;
+  // price: any;
+  // qty: any;
+  // productId: number;
   id?: number;
   product: Products;
   quantity: number;
