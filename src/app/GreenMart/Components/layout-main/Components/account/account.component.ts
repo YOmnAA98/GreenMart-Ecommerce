b785d3a3c19238ef9af902a2ef39e451350c16b6
Mahmoud12345678900000 <mahmoudmamdouh856@gmail.com>
2024-10-21 00:33:07 +0300

    update regester

--- src/app/GreenMart/Components/layout-main/Components/account/account.component.ts
+++ src/app/GreenMart/Components/layout-main/Components/account/account.component.ts
@@ -1,37 +1,3 @@
-// import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-// import { AppComponent } from "../../../../../app.component";
-// import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-// import { CommonModule } from '@angular/common';
-
-// @Component({
-//   selector: 'app-account',
-//   standalone: true,
-//   imports: [AppComponent, RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
-//   templateUrl: './account.component.html',
-//   styleUrl: './account.component.css'
-// })
-// export class AccountComponent implements OnInit {
-
-//   @Output() textChange = new EventEmitter<string>();
-
-//   headerText: string = 'My account'; 
-
-//   constructor() { }
-
-//   ngOnInit(): void {}
-
-//   onLinkClick(newText: string): void {
-//     this.textChange.emit(newText);  
-//     this.updateHeader(newText);     
-//   }
-
-//   updateHeader(newText: string): void {
-//     this.headerText = newText;
-//   }
-// }
-
-
-
 
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
