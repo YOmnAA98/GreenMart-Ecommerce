39f595dd0729a69975ce4918841fcf31c56321c4
YOmnAA98 <yomna.raafat98@gmail.com>
2024-10-21 19:48:22 +0300

    form validation done

--- src/app/GreenMart/Components/layout-main/Components/checkout/checkout.component.ts
+++ src/app/GreenMart/Components/layout-main/Components/checkout/checkout.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators,} from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { ShoppingCartService } from '../../../../Shared/Services/shopping-cart.service';
 import { Cart } from '../../../../Shared/Interfaces/cart';
